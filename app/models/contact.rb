ce44b9af260a53cb52aa1847860e219e342bf9c9
yorgos <79940850+georgtsak@users.noreply.github.com>
2024-02-11 00:04:12 +0200

    Update contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
-  belongs_to :initiator, class_name: 'User'
-  belongs_to :recipient, class_name: 'User'
-end  
+  belongs_to :initiator, class_name: "User"
+  belongs_to :recipient, class_name: "User"
+
+  def self.add_friend(initiator_id, recipient_id)
+    unless exists?(initiator_id: initiator_id, recipient_id: recipient_id) || exists?(initiator_id: recipient_id, recipient_id: initiator_id) #ean einai hdh filos
+    create(initiator_id: initiator_id, recipient_id: recipient_id)
+    end
+  end
+end
